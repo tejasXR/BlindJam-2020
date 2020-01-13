43b6fd7ab10df9dbd1a4e7c3c9f9ca3b55deb453
tejasVR <tejasshroff24@gmail.com>
2020-01-12 19:49:33 -0500

    Adding a host of sounds effects

---
+++ Assets/App/Scripts/Footsteps.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace APERION.BlindJam
+{
+    public class Footsteps : MonoBehaviour
+    {
+
+        [SerializeField] float footstepDistanceThreshold;
+        [SerializeField] AudioSource footStepAudioSource;
+        [SerializeField] AudioClip[] footstepClips;
+
+        private Vector3 origin;
+        private bool footstepsOn;
+
+        private void OnEnable()
+        {
+            StartPoint.StartedCallback += EnableFootsteps;
+        }
+
+        private void OnDisable()
+        {
+            StartPoint.StartedCallback -= EnableFootsteps;
+        }
+
+        void Update()
+        {
+            // If footsteps are enabled
+            if (footstepsOn)
+            {
+                // If we cross a distance threshold
+                if (Vector3.Distance(origin, transform.position) > footstepDistanceThreshold)
+                {
+                    TakeStep();
+                }
+            }           
+        }
+
+        private void EnableFootsteps()
+        {
+            footstepsOn = true;
+            origin = transform.position;
+        }
+
+        private void TakeStep()
+        {
+            // Re-orient origin
+            origin = transform.position;
+
+            // Play random footstep clip
+            var randomClip = footstepClips[Utility.GetRandomInt(0, footstepClips.Length)];
+            footStepAudioSource.PlayOneShot(randomClip);
+        }
+    }
+}
+
+
